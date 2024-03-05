12180745745eaa3ea01d9dff010a3d5c6a7da194
lorenzo-lipp <lorenzo1lipp@gmail.com>
2024-03-05 08:43:11 -0300

    Moving project to its own repository

---
+++ README.md
@@ -0,0 +1,23 @@
+# World Cup Database
+
+- This World Cup database was built using a shell script to insert data from a CSV file into a PostgreSQL database.
+
+Technologies used: PostgreSQL, Shell Script
+
+# Live Version Output
+
+![image](./images/preview-1.png)
+
+![image](./images/preview-2.png)
+
+![image](./images/preview-3.png)
+
+![image](./images/preview-4.png)
+
+![image](./images/preview-5.png)
+
+![image](./images/preview-6.png)
+
+# Requirements
+
+![image](./images/requirements.png)
---
+++ insert_data.sh
@@ -0,0 +1,41 @@
+#! /bin/bash
+
+if [[ $1 == "test" ]]
+then
+  PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
+else
+  PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
+fi
+
+# Do not change code above this line. Use the PSQL variable above to query your database.
+
+# Resets table
+echo "$($PSQL "TRUNCATE TABLE games, teams")"
+
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+do
+  if [[ $YEAR != 'year' ]]
+  then
+    
+    # If winner is not in teams table
+    WINNER_ID="$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")"
+    if [[ -z $WINNER_ID ]]
+    then
+      # Add it
+      INSERT="$($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")"
+      WINNER_ID="$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")"
+    fi
+
+    # If opponent is not in teams table
+    OPPONENT_ID="$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")"
+    if [[ -z $OPPONENT_ID ]]
+    then
+      # Add it
+      INSERT="$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")"
+      OPPONENT_ID="$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")"
+    fi
+
+    # Add game to games table
+    INSERT="$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS)")"
+  fi
+done
